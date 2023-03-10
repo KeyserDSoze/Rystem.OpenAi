dc21c858fcf9c652d4e821fb80886819369b2f75
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-03-10 01:38:56 +0100

    1.0.2

--- src/Rystem.OpenAi.Api/Settings/OpenAiAzureManagedIdentitySettings.cs
+++ src/Rystem.OpenAi.Api/Settings/OpenAiAzureManagedIdentitySettings.cs
@@ -2,7 +2,13 @@
 {
     public sealed class OpenAiAzureManagedIdentitySettings
     {
+        /// <summary>
+        /// Set the ClientId from <see href="https://learn.microsoft.com/en-us/azure/automation/add-user-assigned-identity">user assigned managed identity</see>
+        /// </summary>
         public string? Id { get; set; }
+        /// <summary>
+        /// Set true to use the <see href="https://learn.microsoft.com/en-us/azure/automation/enable-managed-identity-for-automation">system assigned managed identity</see>
+        /// </summary>
         public bool UseDefault { get; set; }
     }
 }
