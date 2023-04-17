76b8ea5cac32a8167aa4b0830864b3c95b881d3e
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-04-17 22:29:45 +0200

    cost tests updated

--- src/Rystem.OpenAi.Test/Tests/CostTests.cs
+++ src/Rystem.OpenAi.Test/Tests/CostTests.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using Rystem.OpenAi.Chat;
 using Xunit;
 
@@ -34,47 +37,58 @@ namespace Rystem.OpenAi.Test
             Assert.Equal(times * numberOfTokens * currentPrice / 1_000, manualCalculatedPrice);
         }
         [Theory]
-        [InlineData("", 1, "This model is the perfect model for you.", 9, 0.002, 3, 5)]
-        [InlineData("", 3, "Multiple models, each with different capabilities and price points. Prices are per 1000 tokens. You can think of tokens as pieces of words, where 1000 tokens is about 750 words.", 40, 0.002, 3, 5)]
-        [InlineData("", 7, "Multiple models, each with different capabilities and price points. Prices are per 1000 tokens. You can think of tokens as pieces of words, where 1000 tokens is about 750 words.", 40, 0.002, 3, 5)]
-        public async Task Gpt3_5(string name, int times, string value, int numberOfTokens, decimal currentPrice, int fixedTokens, int startingAndEndingTokens)
+        [InlineData(ModelFamilyType.Gpt3_5, OpenAiType.Chat, "", 1, "This model is the perfect model for you.", 9, 0.002, 3, 5, "Gpt35Turbo")]
+        [InlineData(ModelFamilyType.Gpt3_5, OpenAiType.Chat, "", 3, "Multiple models, each with different capabilities and price points. Prices are per 1000 tokens. You can think of tokens as pieces of words, where 1000 tokens is about 750 words.", 40, 0.002, 3, 5, "Gpt35Turbo")]
+        [InlineData(ModelFamilyType.Gpt3_5, OpenAiType.Chat, "", 7, "Multiple models, each with different capabilities and price points. Prices are per 1000 tokens. You can think of tokens as pieces of words, where 1000 tokens is about 750 words.", 40, 0.002, 3, 5, "Gpt35Turbo")]
+        public async Task Gpt3_5(ModelFamilyType model, OpenAiType type, string name, int times, string content, int numberOfTokens, decimal currentPrice, int startingAndEndingTokens, int fixedTokens, string modelType)
         {
             var promptedTokens = times * numberOfTokens + (times * fixedTokens) + startingAndEndingTokens;
             var openAiApi = _openAiFactory.Create(name);
             var manualCostCalculator = _openAiCost.Configure(configuration =>
             {
                 configuration
-                    .WithFamily(ModelFamilyType.Gpt3_5)
-                    .WithType(OpenAiType.Chat);
+                    .WithFamily(model)
+                    .WithType(type);
             });
             var manualCalculatedPrice = manualCostCalculator.Invoke(new OpenAiUsage
             {
                 PromptTokens = promptedTokens,
             });
-            Assert.NotNull(openAiApi.Chat);
-            var chat = openAiApi.Chat
-                .Request(new ChatMessage { Role = ChatRole.User, Content = value })
-                .WithModel(ChatModelType.Gpt35Turbo0301)
-                .WithTemperature(1);
-            if (times > 1)
-                for (var i = 1; i < times; i++)
-                    chat.AddMessage(new ChatMessage { Role = ChatRole.User, Content = value });
-            var price = chat
-                .CalculateCost();
+            var responseWithCost = await ExecuteRequestWithCostAsync(openAiApi, type, content, times, modelType);
+            var price = responseWithCost.CostCalculatedBySystem.Invoke();
             Assert.Equal(promptedTokens * currentPrice / 1_000, price);
             Assert.Equal(manualCalculatedPrice, price);
-            var responseWithCost = await chat.ExecuteAndCalculateCostAsync();
-            var finalPriceForEntireOperation = responseWithCost.CalculateCost();
-            var tokenizer = _utility.Tokenizer.WithChatModel(ChatModelType.Gpt35Turbo);
+            var finalPriceForEntireOperation = responseWithCost.CostCalculation.Invoke();
+            var tokenizer = responseWithCost.Tokenizer;
             var tokenForResponse = 0;
-            foreach (var content in responseWithCost.Result.Choices.Select(x => x.Message.Content))
-                tokenForResponse += tokenizer.Encode(content).NumberOfTokens;
+            foreach (var message in responseWithCost.Contents)
+                tokenForResponse += tokenizer.Encode(message).NumberOfTokens;
             var manualFinalCalculatedPrice = manualCostCalculator.Invoke(new OpenAiUsage
             {
-                PromptTokens = numberOfTokens * times,
+                PromptTokens = promptedTokens,
                 CompletionTokens = tokenForResponse
             });
             Assert.True(finalPriceForEntireOperation > price);
+            Assert.Equal(finalPriceForEntireOperation, manualFinalCalculatedPrice);
+        }
+        private async Task<(Func<decimal> CostCalculatedBySystem, Func<decimal> CostCalculation, List<string> Contents, int PromptTokens, int CompletionTokes, IOpenAiTokenizer Tokenizer)> ExecuteRequestWithCostAsync(IOpenAiApi openAiApi, OpenAiType type, string content, int times, string modelType)
+        {
+            switch (type)
+            {
+                case OpenAiType.Chat:
+                    var model = (ChatModelType)Enum.Parse(typeof(ChatModelType), modelType);
+                    var chat = openAiApi.Chat
+                     .Request(new ChatMessage { Role = ChatRole.User, Content = content })
+                     .WithModel(model)
+                     .WithTemperature(1);
+                    if (times > 1)
+                        for (var i = 1; i < times; i++)
+                            chat.AddMessage(new ChatMessage { Role = ChatRole.User, Content = content });
+                    var responseWithCost = await chat.ExecuteAndCalculateCostAsync();
+                    var tokenizer = _utility.Tokenizer.WithChatModel(model);
+                    return (() => chat.CalculateCost(), () => responseWithCost.CalculateCost(), responseWithCost.Result.Choices.Select(x => x.Message.Content).Where(x => x != null).ToList(), responseWithCost.Result.Usage.PromptTokens.Value, responseWithCost.Result.Usage.CompletionTokens.Value, tokenizer);
+            }
+            return default;
         }
     }
 }
