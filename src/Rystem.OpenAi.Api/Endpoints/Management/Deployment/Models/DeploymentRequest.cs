da38321bd6a98a9d8609b58d58cc380e2733834d
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-04-25 22:29:14 +0200

    v. 3.0.0

--- src/Rystem.OpenAi.Api/Endpoints/Management/Deployment/Models/DeploymentRequest.cs
+++ src/Rystem.OpenAi.Api/Endpoints/Management/Deployment/Models/DeploymentRequest.cs
@@ -2,12 +2,11 @@
 
 namespace Rystem.OpenAi.Management
 {
-#warning add State for everything that has State
     public sealed class DeploymentRequest : IOpenAiRequest
     {
         [JsonPropertyName("scale_settings")]
         public DeploymentScaleSettings ScaleSettings { get; set; }
         [JsonPropertyName("model")]
-        public string ModelId { get; set; }
+        public string? ModelId { get; set; }
     }
 }
