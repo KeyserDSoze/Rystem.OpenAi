01c6b6695e63da713ed606b0c3e3735b9a8c0901
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-08-03 19:00:29 +0200

    ILogger preview

---
+++ src/Rystem.OpenAi.Api/Utilities/Logs/IOpenAiLogger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Rystem.OpenAi.Api.Utilities.Logs
+{
+    public interface IOpenAiLogger
+    {
+        ILogger<IOpenAiLogger> Custom { get; }
+    }
+}
---
+++ src/Rystem.OpenAi.Api/Utilities/Logs/OpenAiLogger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Rystem.OpenAi.Api.Utilities.Logs
+{
+    internal sealed class OpenAiLogger : IOpenAiLogger
+    {
+        public ILogger<IOpenAiLogger> Custom { get; }
+        public OpenAiLogger(ILogger<IOpenAiLogger> logger)
+        {
+            Custom = logger;
+        }
+        public async Task LogAsync(Func<Task> toExecuteWithLog, string message)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            Custom.LogInformation($"{message} starts.");
+            try
+            {
+                await toExecuteWithLog().ConfigureAwait(false);
+                timer.Stop();
+                Custom.LogInformation($"{message} ends.", timer);
+            }
+            catch (Exception ex)
+            {
+                timer.Stop();
+                Custom.LogError(ex.Message, $"{message} in error.", timer);
+                throw;
+            }
+        }
+        public void Log(Action toExecuteWithLog, string message)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            Custom.LogInformation($"{message} starts.");
+            try
+            {
+                toExecuteWithLog();
+                timer.Stop();
+                Custom.LogInformation($"{message} ends.", timer);
+            }
+            catch (Exception ex)
+            {
+                timer.Stop();
+                Custom.LogError(ex.Message, $"{message} in error.", timer);
+                throw;
+            }
+        }
+    }
+}
