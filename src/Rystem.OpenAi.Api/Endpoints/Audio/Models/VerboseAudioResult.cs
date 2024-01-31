d3ac4483a9398a1048b39d425cbbb49e0d4d4dfa
Tomas Ekeli <te@davidhorn.com>
2024-01-31 09:40:06 +0000

    improve types of properties on VerboseAudioResult
    The times are now TimeSpans and we don't need full doubles for the
    temperature, compression-ratio and probabilities

--- src/Rystem.OpenAi.Api/Endpoints/Audio/Models/VerboseAudioResult.cs
+++ src/Rystem.OpenAi.Api/Endpoints/Audio/Models/VerboseAudioResult.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json.Serialization;
 
 namespace Rystem.OpenAi.Image
@@ -11,7 +12,7 @@ namespace Rystem.OpenAi.Image
         public string? Language { get; set; }
 
         [JsonPropertyName("duration")]
-        public double Duration { get; set; }
+        public TimeSpan Duration { get; set; }
 
         [JsonPropertyName("segments")]
         public AudioSegment[]? Segments { get; set; }
@@ -22,13 +23,13 @@ namespace Rystem.OpenAi.Image
             public int Id { get; set; }
 
             [JsonPropertyName("seek")]
-            public int Seek { get; set; }
+            public TimeSpan Seek { get; set; }
 
             [JsonPropertyName("start")]
-            public double Start { get; set; }
+            public TimeSpan Start { get; set; }
 
             [JsonPropertyName("end")]
-            public double End { get; set; }
+            public TimeSpan End { get; set; }
 
             [JsonPropertyName("text")]
             public string? Text { get; set; }
@@ -37,16 +38,16 @@ namespace Rystem.OpenAi.Image
             public int[]? Tokens { get; set; }
 
             [JsonPropertyName("temperature")]
-            public double Temperature { get; set; }
+            public float Temperature { get; set; }
 
             [JsonPropertyName("avg_logprob")]
-            public double AvgLogprob { get; set; }
+            public float AvgLogprob { get; set; }
 
             [JsonPropertyName("compression_ratio")]
-            public double CompressionRatio { get; set; }
+            public float CompressionRatio { get; set; }
 
             [JsonPropertyName("no_speech_prob")]
-            public double NoSpeechProb { get; set; }
+            public float NoSpeechProb { get; set; }
         }
     }
 }
