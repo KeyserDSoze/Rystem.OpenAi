b3b6aa782530fa2180d6b3a2ea45aa16a4a49144
keyserdsoze <alessandro.rapiti44@gmail.com>
2024-12-14 17:46:10 +0100

    Add interfaces and classes for file management and search
    This commit introduces several new interfaces and classes in the
    `Rystem.OpenAi.Assistant` namespace to enhance the functionality
    of the OpenAI assistant. Key additions include:
    
    - `IOpenAiToolResourcesAssistant` for managing code interpreters
      and vector stores.
    - `IOpenAiFileSearchToolResourcesAssistant` for file search
      tool resources, including metadata management and chunking
      strategies.
    - `IOpenAiFileSearchAssistant` for defining file search ranking
      options.
    - `AssistantCodeInterpreterToolResources` and
      `AssistantFileSearchToolResources` for managing file IDs and
      vector store IDs.
    - `AssistantRequest` class implementing `IOpenAiRequest` with
      properties for request management.
    - `RankerName` class for specific ranking options in file searches.
    
    These changes significantly improve the assistant's capabilities
    in file management and search functionalities.

---
+++ src/Rystem.OpenAi/Endpoints/Assistant/Models/RankerName.cs
@@ -0,0 +1,9 @@
+﻿namespace Rystem.OpenAi.Assistant
+{
+    public sealed class RankerName : ModelName
+    {
+        internal RankerName(string name) : base(name) { }
+        public static RankerName Auto { get; } = new("auto");
+        public static RankerName Default_2024_08_21 { get; } = new("default_2024_08_21");
+    }
+}
